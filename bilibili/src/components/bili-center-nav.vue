3eb16b7337468c783316087bc15ff415f1d9ba9a
Jeang <2609231566@qq.com>
2026-05-26 23:16:51 +0800

    bilibili

---
+++ bilibili/src/components/bili-center-nav.vue
@@ -0,0 +1,83 @@
+<script setup>
+import "@/style/bili-center-nav.css";
+</script>
+
+<template>
+  <div class="center-box">
+    <div class="left">
+      <div class="img-wrap">
+        <div class="img">
+          <img src="@/assets/dongtai.svg" alt="" />
+        </div>
+        <span>动态</span>
+      </div>
+      <div class="img-wrap">
+        <div class="img img2">
+          <img src="@/assets/hot.svg" alt="" />
+        </div>
+        <span>热门</span>
+      </div>
+    </div>
+    <div class="center">
+      <ul>
+        <li>番剧</li>
+        <li>国创</li>
+        <li>综艺</li>
+        <li>动画</li>
+        <li>鬼畜</li>
+        <li>舞蹈</li>
+        <li>娱乐</li>
+        <li>科技数码</li>
+        <li>美食</li>
+        <li>汽车</li>
+        <li>体育运动</li>
+        <li>vlog</li>
+        <li>人工智能</li>
+        <li>户外生活</li>
+        <li>电影</li>
+        <li>电视剧</li>
+        <li>纪录片</li>
+        <li>游戏</li>
+        <li>音乐</li>
+        <li>影视</li>
+        <li>知识</li>
+        <li>资讯</li>
+        <li>小剧场</li>
+        <li>时尚美妆</li>
+        <li>动物</li>
+        <li>绘画</li>
+        <li>家装房产</li>
+        <li>更多</li>
+      </ul>
+    </div>
+    <div class="right">
+      <span class="item">
+        <img src="@/assets/zhuanlan.svg" alt="">
+        <span>专栏</span>
+      </span>
+      <span class="item">
+        <img src="@/assets/huodong.svg" alt="">
+        <span>活动</span>
+      </span>
+      <span class="item">
+        <img src="@/assets/shequ.svg" alt="">
+        <span>社区中心</span>
+      </span>
+      <span class="item">
+        <img src="@/assets/zhibo.svg" alt="">
+        <span>直播</span>
+      </span>
+      <span class="item">
+        <img src="@/assets/ketang.svg" alt="">
+        <span>课堂</span>
+      </span>
+      <span class="item">
+        <img src="@/assets/xinge.svg" alt="">
+        <span>新歌热榜</span>
+      </span>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="less">
+</style>
